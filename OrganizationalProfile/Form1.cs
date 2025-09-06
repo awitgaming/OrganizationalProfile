507c9f3ee513a510251e4f45b70ca94340f21dd5
awitgaming <156307787+awitgaming@users.noreply.github.com>
2025-09-06 19:40:44 +0800

    Add project files.

---
+++ OrganizationalProfile/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrganizationalProfile
+{
+    public partial class OrganizationalProfile : Form
+    {
+        public OrganizationalProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
